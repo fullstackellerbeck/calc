925c3ca2a0a286590d445e8feb534f077a41f21c
Full Stack Ellerbeck <chris@fullstackellerbeck.com>
2016-01-09 05:41:46 -0500

    Bracket fixes
    - Problems with adding or subtracting brackets from brackets

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
-<?php 	require_once( "lib/calc.php" );	?>
 <!DOCTYPE html>
-<html lang="en" ng-app>
+<html lang="en">
 	<head>
-
+		<title>Challenge Solution</title>
+<?php 	require_once( "lib/calc.php" );	?>
 	</head>
 
 	<body>
 		<h1>Challenge Solution</h1>
 		Enter an equation below, using only brackets, + or -<br />
 		<form action="index.php" method="post">
-			<input type="text" name="sub_equation" style="width: 400px;" value="<?php echo( $_POST[ "sub_equation" ] ); ?>" />
+			<input type="text" name="sub_equation" style="width: 400px;" id="equInput" value="<?php echo( $_POST[ "sub_equation" ] ); ?>" />
 			<input type="submit" name="sub_button" value="Solve it!" />
 		</form>
 		<br />
@@ -18,5 +18,12 @@
   			<b>The answer is:</b><br />
 			<?php echo( $_POST[ "sub_equation" ] ); ?> = <b><?php echo( evaluate( $_POST[ "sub_equation" ] ) ); ?></b>
 <?php	}	?>
+
+		<script type="text/javascript">
+			var eqElement = document.getElementById('equInput');
+			
+			eqElement.focus();
+			eqElement.value = eqElement.value;
+		</script>
   	</body>
 </html>
--- lib/calc.php
+++ lib/calc.php
@@ -1,25 +1,29 @@
  <?php
 
     /* 
-       Name: evaluate
-       Author: Chris Ellerbeck
-       Date: January 8 2016
-
-       This function converts the passed expression to an array, then iterates through the array looking for the following
-       characters: - + (
-
-       If + or - are found it notes what the operation is, then continues looping through the array, if the next character
-       is a number it uses the previous operation on the current number and tallies up the result.
-
-       If a ( is encountered it will seperately iterate through the bracketed section looking for a ). If this is found
-       it recursively calls itself with the equation from within the brackets, returns the result, and depending on the 
-       operator before the ( with either add or subtract the result.
-
-       This is based off of a Hacker Rank challenge that was asked of me, and I completely mangled. I had to try to solve
-       it, and since Google is returning the same answer as the below code, I would say it's working!
-
-       Thanks!
-       Chris
+     *
+     * Name: evaluate
+     * Author: Chris Ellerbeck
+     * Date: January 9 2016
+     *
+     * This function converts the passed expression to an array, then iterates through the array looking for the following
+     * characters: - + (
+     *
+     * If + or - are found it notes what the operation is, then continues looping through the array, if the next character
+     * is a number it uses the previous operation on the current number and tallies up the result.
+     *
+     * If a ( is encountered it will seperately iterate through the bracketed section looking for a ). If this is found
+     * it recursively calls itself with the equation from within the brackets, returns the result, and depending on the 
+     * operator before the ( with either add or subtract the result.
+     *
+     * This is based off of a Hacker Rank challenge that was asked of me, one which I completely mangled. I was a little
+     * disappointed in myself, so I had to try to solve it, and since Google and WolframAlpha is returning the same answer
+     * as the below code, I would say it's working! Until someone does something that the function doesn't expect and it loses
+     * it's mind :)
+     *
+     * Thanks,
+     * Chris
+     *
      */
  
     function evaluate( $expression ) {
@@ -35,13 +39,12 @@
             } elseif( $expArr[ $cnt ] == "-" ) {
                 $prevOp = "-";
             } elseif( $expArr[ $cnt ] == "(" ) {
+
                 for( $brCnt = $cnt; $brCnt < count( $expArr ); $brCnt++ ) {
                     if( $expArr[ $brCnt ] == ")" ) {
                         $inExp = "";
 
-                        for( $inCnt = ($cnt + 1); $inCnt < $brCnt; $inCnt++ ) {
-                            $inExp = $inExp . $expArr[ $inCnt ];
-                        }
+                        for( $inCnt = ($cnt + 1); $inCnt < $brCnt; $inCnt++ ) $inExp = $inExp . $expArr[ $inCnt ];
 
                         $bracketNum = evaluate( $inExp );
 
@@ -55,6 +58,12 @@
                         break;                        
                     }
                     
+                }
+
+                if( $expArr[ $cnt ] == "+" ) {
+                    $prevOp = "+";
+                } elseif( $expArr[ $cnt ] == "-" ) {
+                    $prevOp = "-";
                 }
             } else {
                 $workingNum = $expArr[ $cnt ];
